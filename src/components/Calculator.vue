375cfef27a095e0a8959ed0cd2a5e911fa049732
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-04-12 12:58:09 +0800

    Setup init component

---
+++ src/components/Calculator.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="calculator">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
